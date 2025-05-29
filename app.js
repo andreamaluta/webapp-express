05fb4797b3a9599f70ec8e489caa5217b9f98806
andreamaluta <andreamaluta@gmail.com>
2025-05-29 14:42:52 +0200

    [UPD] imported the router in app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 
 const port = process.env.SERVER_PORT || 3000;
 
+const filmRouter = require('./router/films.js');
+app.use('/films', filmRouter)
+
 app.get("/", (req, res) => {
     res.send('Hello World!')
 })

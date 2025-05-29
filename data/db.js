9915c12f23f93dade236cf7a49bbc9d9bf5731b9
andreamaluta <andreamaluta@gmail.com>
2025-05-29 14:31:06 +0200

    [UPD] use the environment variables

--- data/db.js
+++ data/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-    host: "",
-    port: "",
-    user: "",
-    password: "",
-    database: ""
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
 
 connection.connect((err) => {

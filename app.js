5c7c61707618bf2d18bec32cb039726ec68a7c12
andreamaluta <andreamaluta@gmail.com>
2025-06-03 14:49:45 +0200

    [UPD] imported cors

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const cors = require('cors')
+
 const app = express();
 
 const port = process.env.SERVER_PORT || 3000;
+
+app.use(cors({ origin: process.env.FE_APP }));
 
 const filmRouter = require('./router/films.js');
 app.use('/films', filmRouter)
